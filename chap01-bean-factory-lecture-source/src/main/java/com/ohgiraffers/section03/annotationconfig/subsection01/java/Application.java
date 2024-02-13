e9f0beb920c980a88dd306294e0fb0c799ddf695
yerincho06 <yerincho06@gmail.com>
2024-02-13 12:20:35 +0900

    의존성 주입

--- chap01-bean-factory-lecture-source/src/main/java/com/ohgiraffers/section03/annotationconfig/subsection01/java/Application.java
+++ chap01-bean-factory-lecture-source/src/main/java/com/ohgiraffers/section03/annotationconfig/subsection01/java/Application.java
@@ -5,8 +5,19 @@ package com.ohgiraffers.section03.annotationconfig.subsection01.java;
  *  이 중에서도 java클래스와 @ComponentScan 방식을 자주 사용하고 있다.
 **/
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Application {
     public static void main(String[] args) {
+        ApplicationContext context
+                = new AnnotationConfigApplicationContext(ContextConfiguration.class);
+
+        String[] beanNames = context.getBeanDefinitionNames();
+        for (String beanName : beanNames) {
+            System.out.println("beanName = " + beanName);
+
+        }
 
     }
 }
